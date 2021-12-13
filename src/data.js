327fdcc7f166ff8e62a14d4c42129e9d6d06b47a
Deni Mazzei <dconkell@gmail.com>
2021-12-13 14:54:10 -0600

    updated image directory for projects

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "Project 1",
     description:
       "The goal of this project was to develop an application that will provide results about nearby local coffee shops in a given radius from the user’s location. Upon result selection, app will provide navigational assistance to that location as an alternative to Starbucks in an effort to support the local community.",
-    image: "./group-project-1.png",
+    image: "./assets/group-project-1.png",
     deployed: "https://denimazzei.github.io/stick-it-to-starbucks-project-1/",
     github: "https://github.com/denimazzei/stick-it-to-starbucks-project-1",
   },
@@ -13,7 +13,7 @@ export const projects = [
     subtitle: "Homework",
     description:
       "Create javascript code to generate a safe, randomized password for password",
-    image: "./password-generator.png",
+    image: "./assets/password-generator.png",
     deployed: "https://denimazzei.github.io/Deni-Password-Generator-hw3/",
     github: "https://github.com/denimazzei/Deni-Password-Generator-hw3",
   },
@@ -22,7 +22,7 @@ export const projects = [
     subtitle: "Homework",
     description:
       "A daily work-day scheduler with  hourly task rows using Moment.js",
-    image: "./workday-scheduler.png",
+    image: "./assets/workday-scheduler.png",
     deployed: "https://denimazzei.github.io/Deni-Workday-Scheduler-hw5/",
     github: "https://github.com/denimazzei/Deni-Workday-Scheduler-hw5",
   },
@@ -31,7 +31,7 @@ export const projects = [
     subtitle: "Project 2",
     description:
       "Can you get the emergency supplies safely across the universe? See if you can clear a path through the astroid field by playing this game made with p5.js, MySQL, and Express.js.",
-    image: "./SpaceEMS.png",
+    image: "./assets/SpaceEMS.png",
     deployed: "https://space-ems.herokuapp.com/",
     github: "https://github.com/denimazzei/Space-EMS",
   },
