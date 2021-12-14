08f960f4a88e578992dfa38fc81ff51609a12799
Deni Mazzei <dconkell@gmail.com>
2021-12-13 18:30:38 -0600

    work on project.js section divs

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,7 +18,7 @@ export default function Projects() {
         <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
             <a
-              href={project.link}
+              href={project.deployed}
               key={project.image}
               className="sm:w-1/2 w-100 p-4"
             >
@@ -35,7 +35,12 @@ export default function Projects() {
                   <h1 className="title-font text-lg font-medium text-white mb-3">
                     {project.title}
                   </h1>
-                  <p className="leading-relaxed">{project.description}</p>
+                  <p className="title-font font-medium text-white">
+                    {project.description}
+                  </p>
+                  <a className="leading-relaxed" href={project.github}>
+                    Github
+                  </a>
                 </div>
               </div>
             </a>
