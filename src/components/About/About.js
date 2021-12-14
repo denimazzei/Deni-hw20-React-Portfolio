af5afa76ddb3984b5a30dca10b53a60b1d129832
Deni Mazzei <dconkell@gmail.com>
2021-12-13 21:52:19 -0600

    updated file structure for component pages

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function About() {
   return (
-    <section id="about">
+    <container id="about">
       <div className="container mx-auto flex px-10 py-20 md: flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4.1 text-3.1 mb-4 font-medium text-white">
@@ -42,6 +42,6 @@ export default function About() {
           />
         </div>
       </div>
-    </section>
+    </container>
   );
 }
