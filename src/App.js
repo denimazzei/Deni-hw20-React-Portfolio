8f040c99ccd16df671adb905c5f8a461824cd8a6
Deni Mazzei <dconkell@gmail.com>
2021-12-15 23:39:11 -0600

    fixing errors with routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
-import Projects from "./components/Portfolio/Projects";
+import React from "react";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/Home/home";
+import About from "./components/About/About";
+import Projects from "./components/Portfolio/Projects";
+import Contact from "./components/Contact/Contact";
 
-const routes = [
-  {
-    path: "/contact",
-    component: Contact,
-  },
-  {
-    path: "/about",
-    component: About,
-  },
-  {
-    path: "/projects",
-    component: Projects,
-  },
-  {
-    path: "/",
-    component: Home,
-  },
-];
+function App() {
+  return (
+    <Router>
+      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Routes exact path="/" component={Home} />
+        <Routes exact path="/about" component={About} />
+        <Routes exact path="/projects" component={Projects} />
+        <Routes exact path="/contact" component={Contact} />
+      </div>
+    </Router>
+  );
+}
 
-export default routes;
+export default App;
--- src/pages.js
+++
@@ -1,3 +0,0 @@
-export { default as Projects } from "./home";
-export { default as About } from "./about";
-export { default as Contact } from "./contact";
